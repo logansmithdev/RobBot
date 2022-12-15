1e56779b0a15945ef95f1b859a5f32c34570782b
smithlp <lsmith@bdpest.onmicrosoft.com>
2022-12-15 14:46:05 -0500

    added tts and channel join announcement

--- bot.py
+++ bot.py
@@ -1,9 +1,11 @@
 import discord
 import os
+import asyncio
+from gtts import gTTS
 from dotenv import load_dotenv
 from discord.ext import commands
 from gtts import gTTS
-
+from mutagen.mp3 import MP3
 load_dotenv()
 
 bot = commands.Bot(command_prefix='.', intents=discord.Intents.all())
@@ -20,5 +22,40 @@ async def hello(ctx):
     if ctx.author == bot.user:
         return
     await ctx.send('Hello!')
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    voice_client = discord.utils.get(bot.voice_clients, guild=member.guild)
+    if before.channel != after.channel and member.id != bot.application_id and not (voice_client and voice_client.is_connected()):
+        if(after.channel is None and before.channel and before.channel.members and len(before.channel.members) > 0):
+            message = member.display_name + " disconnected"
+            await playTTS(before.channel, message)
+        elif after.channel:
+            message = member.display_name + " joined " + after.channel.name
+            await playTTS(after.channel, message)
+
+
+@bot.command()
+async def tts(ctx, *, text):
+    # Checking to make sure the bot (client.user) is not the one that sent the message (message.author)
+    if ctx.author == bot.user:
+        return
+    voice_client = discord.utils.get(bot.voice_clients, guild=ctx.author.guild)
+    if voice_client and voice_client.is_connected():
+        await ctx.send("Please wait until I am finished speaking.")
+        return
+    await playTTS(ctx.author.voice.channel, text)
+
+async def playTTS(voiceChannel, text):
+    vc = await voiceChannel.connect()
+    speech = gTTS(text = text, lang = 'en', slow = False)
+    cwd = os.getcwd()
+    audioFilePath = cwd + "/audio.mp3"
+    speech.save(audioFilePath)
+    vc.play(discord.FFmpegPCMAudio(audioFilePath), after=lambda e: print("done playing tts"))
+    audio = MP3(audioFilePath)
+    await asyncio.sleep(audio.info.length)
+    await vc.disconnect()
+    
 
 bot.run(token)
